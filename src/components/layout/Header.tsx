eb69b49a06c7512b805683d8737e182527fadf9c
Abhishek Jain <aabhishekjain53@gmail.com>
2026-06-30 16:25:25 +0530

    Header: height exactly matches logo height 110/130px

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -42,7 +42,7 @@ export default function Header() {
         className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${headerBg}`}
       >
         <div className="container-site">
-          <div className="flex items-center justify-between h-[118px] md:h-[138px]">
+          <div className="flex items-center justify-between h-[110px] md:h-[130px]">
 
             {/* Logo */}
             <Link
